9cf438fac3dc116ca8b4f81c55c48d0c14121c5c
aasawariMongoDB <103485554+aasawariMongoDB@users.noreply.github.com>
2024-06-24 12:52:40 +0530

    Adding files for making connection with MongoDB

--- src/main/java/org/minecraft/playermanagement/MongoDBPlugin.java
+++ src/main/java/org/minecraft/playermanagement/MongoDBPlugin.java
@@ -4,42 +4,48 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 import org.bukkit.plugin.java.JavaPlugin;
-
 import java.io.InputStream;
 import java.util.Properties;
 
-    public class MongoDBPlugin extends JavaPlugin {
+public class MongoDBPlugin extends JavaPlugin {
 
-        private MongoDatabase database;
+    private MongoDatabase database;
 
-        @Override
-        public void onEnable() {
-            getLogger().info("MongoDBTest plugin has been enabled");
+    @Override
+    public void onEnable() {
+        getLogger().info("MongoDBTest plugin has been enabled");
 
-            // Load the MongoDB URI from the properties file
-            Properties properties = new Properties();
-            String uri = null;
+        Properties properties = new Properties();
+        String uri ;
 
-            try (InputStream input = getClass().getClassLoader().getResourceAsStream("application.properties")) {
-                if (input == null) {
-                    getLogger().severe("Sorry, unable to find application.properties");
-                    return;
-                }
-                properties.load(input);
-                uri = properties.getProperty("mongo.uri");
-
-                MongoClient mongoClient = MongoClients.create(uri);
-                database = mongoClient.getDatabase("test");
-                getLogger().info("Connected to the database successfully");
-
-            } catch (Exception ex) {
-                ex.printStackTrace();
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("application.properties")) {
+            if (input == null) {
+                getLogger().severe("Sorry, unable to find application.properties");
+                return;
             }
-        }
+            properties.load(input);
+            uri = properties.getProperty("mongo.uri");
 
-        @Override
-        public void onDisable() {
-            getLogger().info("MongoDBTest plugin has been disabled");
+            MongoClient mongoClient = MongoClients.create(uri);
+            database = mongoClient.getDatabase("test");
+            getLogger().info("Connected to the database successfully");
+
+            // Example: Listing collections in the database
+            for (String name : database.listCollectionNames()) {
+                getLogger().info("Collection: " + name);
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
 
+    @Override
+    public void onDisable() {
+        getLogger().info("MongoDBTest plugin has been disabled");
+    }
+
+    public MongoDatabase getDatabase() {
+        return database;
+    }
+}
